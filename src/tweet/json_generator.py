ae6685a5c75c323cf2c2ed9490763f0cb36ca328
CPekko <eirik_fosse2@hotmail.com>
2016-04-11 09:59:50 +0200

    Use positions from DB in tweets.

--- src/tweet/json_generator.py
+++ src/tweet/json_generator.py
@@ -1,6 +1,5 @@
 from tweet.tweet import Tweet
 from position.position import Position
-import random
 
 class Json:
 
@@ -8,12 +7,10 @@ class Json:
     def generate_json(cls, tweets):
         json_tweets = []
         for tweet in tweets:
-            lat = random.uniform(58.1, 70.1)
-            lng = random.uniform(4.6, 30.1)
             json_tweet = {
                 'position': {
-                    'lat': lat,
-                    'lng': lng
+                    'lat': float(tweet.position.latitude),
+                    'lng': float(tweet.position.longitude)
                 },
                 'id': tweet.id
             }
