d1c1130006f46221da939c8b0c089ce8ddbd9c29
Michael McMillan <email@michaelmcmillan.net>
2016-04-11 08:57:19 +0200

    add a repr method in place

--- src/position/place.py
+++ src/position/place.py
@@ -1,5 +1,10 @@
 class Place:
 
-    def __init__(self, name, id):
-        self.name = name
+    def __init__(self, name=None, id=None):
         self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return '<Place:id=%s, name=%s>' % (
+            self.id, self.name
+        )
