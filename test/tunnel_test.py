f9bf44f644b8ab730f342471026d01c6b8a31d3e
Michael McMillan <email@michaelmcmillan.net>
2016-04-12 00:47:57 +0200

    add empty result test

--- test/tunnel_test.py
+++ test/tunnel_test.py
@@ -12,3 +12,11 @@ class TestTunnel(TestCase):
         query = Query(event='injuri', evidence='knife')
         ids = tunnel.get_result_ids(query)
         self.assertEqual(ids, ['1234', '4321'])
+
+    @patch('ontology.tunnel.requests')
+    def test_pull_returns_an_empty_list_of_tweet_ids_if_empty_result(self, requests):
+        requests.get.return_value = MagicMock(text='[]')
+        tunnel = Tunnel()
+        query = Query(event='injuri', evidence='knife')
+        ids = tunnel.get_result_ids(query)
+        self.assertEqual(ids, [])
