e102ae13fabc6b6de9b7d8c588b1cc2b83a265ad
CPekko <eirik_fosse2@hotmail.com>
2016-04-14 15:50:11 +0200

    Added config example.

---
+++ src/config/config_example.py
@@ -0,0 +1,20 @@
+configuration = {
+
+    'database': {
+        'location': '/path/to/tweets.db'
+    },
+
+    'ontology': {
+        'tunnel': 'http://localhost',
+        'port': '8080',
+        'page': 'owlapi',
+        'add_tweets': 'addTweet'
+    },
+
+    'twitter': {
+        'oauth_token': 'token',
+        'oauth_secret': 'secret',
+        'consumer_key': 'key',
+        'consumer_secret': 'secret'
+    }
+}
